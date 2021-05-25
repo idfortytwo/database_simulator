112112089c1259eb9e7aa9edc95c86f4beb051a6
Artem Buhera <artem.bugera@gmail.com>
2021-05-25 02:15:45 +0200

    imports

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -1,7 +1,8 @@
 import sys
 
 from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import QAbstractItemView
+from PyQt5.QtWidgets import QAbstractItemView, QMainWindow, QWidget, QPushButton, QLineEdit, QTableWidget, QStatusBar, \
+    QTableWidgetItem
 
 from db.database import Database
 
@@ -9,7 +10,7 @@ from db.database import Database
 class DatabaseWindow:
     def __init__(self, db: Database):
         self.app = QtWidgets.QApplication(sys.argv)
-        self.main_window = QtWidgets.QMainWindow()
+        self.main_window = QMainWindow()
 
         self.db = db
 
@@ -20,40 +21,40 @@ class DatabaseWindow:
         self.main_window.setObjectName("MainWindow")
         self.main_window.resize(1139, 775)
         # MainWindow.resize(800, 500)
-        self.centralwidget = QtWidgets.QWidget(self.main_window)
+        self.centralwidget = QWidget(self.main_window)
         self.centralwidget.setObjectName("centralwidget")
 
-        self.add_table_button = QtWidgets.QPushButton(self.centralwidget)
+        self.add_table_button = QPushButton(self.centralwidget)
         self.add_table_button.setGeometry(QtCore.QRect(30, 360, 71, 23))
         self.add_table_button.setText('Add table')
 
-        self.remove_table_button = QtWidgets.QPushButton(self.centralwidget)
+        self.remove_table_button = QPushButton(self.centralwidget)
         self.remove_table_button.setGeometry(QtCore.QRect(30, 390, 91, 23))
         self.remove_table_button.setText('Remove table')
 
-        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit = QLineEdit(self.centralwidget)
         self.lineEdit.setGeometry(QtCore.QRect(220, 30, 801, 41))
         self.lineEdit.setObjectName("lineEdit")
-        self.search_button = QtWidgets.QPushButton(self.centralwidget)
+        self.search_button = QPushButton(self.centralwidget)
         self.search_button.setGeometry(QtCore.QRect(1030, 30, 71, 41))
         self.search_button.setObjectName("pushButton_3")
         self.search_button.setText("Search")
 
-        self.table_names_table = QtWidgets.QTableWidget(self.centralwidget)
+        self.table_names_table = QTableWidget(self.centralwidget)
         self.table_names_table.setGeometry(QtCore.QRect(30, 30, 171, 321))
         self.table_names_table.setObjectName("tableWidget")
         self.table_names_table.setColumnCount(1)
         self.table_names_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
         self.table_names_table.cellDoubleClicked.connect(self.refill_table_data)
 
-        self.table_data_table = QtWidgets.QTableWidget(self.centralwidget)
+        self.table_data_table = QTableWidget(self.centralwidget)
         self.table_data_table.setGeometry(QtCore.QRect(220, 80, 881, 661))
         self.table_data_table.setObjectName("tableWidget_2")
         self.table_data_table.setColumnCount(0)
         self.table_data_table.setRowCount(0)
 
         self.main_window.setCentralWidget(self.centralwidget)
-        self.statusbar = QtWidgets.QStatusBar(self.main_window)
+        self.statusbar = QStatusBar(self.main_window)
         self.statusbar.setObjectName("statusbar")
         self.main_window.setStatusBar(self.statusbar)
 
@@ -64,7 +65,7 @@ class DatabaseWindow:
         table_names = self.db.get_table_names()
         self.table_names_table.setRowCount(len(table_names))
         for i, row in enumerate(table_names):
-            item = QtWidgets.QTableWidgetItem(row)
+            item = QTableWidgetItem(row)
             self.table_names_table.setItem(i, 0, item)
 
     def refill_table_data(self, row):
@@ -87,4 +88,4 @@ class DatabaseWindow:
 
     def show(self):
         self.main_window.show()
-        sys.exit(self.app.exec_())
+        self.app.exec()
