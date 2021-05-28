62b471d07ea3d6b84e6c36cfe7a1709dea05c991
Artem Buhera <artem.bugera@gmail.com>
2021-05-28 15:06:34 +0200

    moved AddTableWindow class to new file

--- src/gui/add_table_window.py
+++ src/gui/add_table_window.py
@@ -1,9 +1,6 @@
-import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-from PyQt5 import QtCore, QtWidgets, QtGui
-
-from db.data_types import Integer, Float, Text
-from db.database import Database
+from db import data_types
 from db.exceptions import DuplicateColumnNameError
 from db.table import Table
 
@@ -16,13 +13,8 @@ class TypeComboBox(QtWidgets.QComboBox):
         for data_type in data_types:
             self.addItem(str(data_type))
 
-    # def currentText(self):
-    #     return self.data_types_dict[self.currentText()]
-
 
 class AddTableWindow(QtWidgets.QWidget):
-    db: Database
-
     def __init__(self, main_window):
         super().__init__()
         self.main_window = main_window
@@ -88,7 +80,10 @@ class AddTableWindow(QtWidgets.QWidget):
         self.columns_table.setRowCount(row + 1)
 
         column_name = QtWidgets.QTableWidgetItem()
-        column_types = TypeComboBox([Integer, Float, Text])
+        column_types = TypeComboBox([
+            data_types.Integer,
+            data_types.Float,
+            data_types.Text])
 
         self.columns_table.setItem(row, 0, column_name)
         self.columns_table.setCellWidget(row, 1, column_types)
@@ -148,143 +143,3 @@ class AddTableWindow(QtWidgets.QWidget):
     def closeEvent(self, a0):
         self.main_window.add_table_window = None
         self.close()
-
-
-class ConfirmRemoval(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-
-
-class DatabaseWindow(QtWidgets.QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.db = Database()
-
-        self.add_table_window = None
-        self.setup_UI()
-
-    def setup_UI(self):
-        self.resize(1139, 775)
-        # MainWindow.resize(800, 500)
-        self.centralwidget = QtWidgets.QWidget(self)
-
-        self.add_table_button = QtWidgets.QPushButton(self.centralwidget)
-        self.add_table_button.setGeometry(QtCore.QRect(30, 360, 71, 23))
-        self.add_table_button.setText('Add table')
-        self.add_table_button.clicked.connect(self.add_table)
-
-        # TODO: remove table function
-        # TODO: ask for confirmation
-        self.remove_table_button = QtWidgets.QPushButton(self.centralwidget)
-        self.remove_table_button.setGeometry(QtCore.QRect(30, 390, 91, 23))
-        self.remove_table_button.setText('Remove table')
-
-        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit.setGeometry(QtCore.QRect(220, 30, 801, 41))
-        self.search_button = QtWidgets.QPushButton(self.centralwidget)
-        self.search_button.setGeometry(QtCore.QRect(1030, 30, 71, 41))
-        self.search_button.setText("Search")
-
-        self.table_names_table = QtWidgets.QTableWidget(self.centralwidget)
-        self.table_names_table.setGeometry(QtCore.QRect(30, 30, 171, 321))
-        self.table_names_table.setColumnCount(1)
-        self.table_names_table.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
-        self.table_names_table.cellDoubleClicked.connect(self.refill_table_data)
-        self.table_names_table.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
-
-        self.table_data_table = QtWidgets.QTableWidget(self.centralwidget)
-        self.table_data_table.setGeometry(QtCore.QRect(220, 80, 881, 661))
-        self.table_data_table.setColumnCount(0)
-        self.table_data_table.setRowCount(0)
-
-        self.load_db_button = QtWidgets.QPushButton(self.centralwidget)
-        self.load_db_button.setGeometry(QtCore.QRect(30, 670, 91, 31))
-        self.load_db_button.setText('Load database')
-        self.load_db_button.clicked.connect(self.load_db)
-
-        self.save_db_button = QtWidgets.QPushButton(self.centralwidget)
-        self.save_db_button.setGeometry(QtCore.QRect(30, 710, 91, 31))
-        self.save_db_button.setText('Save database')
-        self.save_db_button.clicked.connect(self.save_db)
-
-        self.setCentralWidget(self.centralwidget)
-        self.statusbar = QtWidgets.QStatusBar(self)
-        self.setStatusBar(self.statusbar)
-
-        self.setWindowTitle('Database Simulator')
-        QtCore.QMetaObject.connectSlotsByName(self)
-
-    def load_db(self):
-        filename = QtWidgets.QFileDialog.getOpenFileName(
-            self, 'Load database', '', 'Database Files (*.db)')[0]
-
-        if not filename:
-            return
-
-        self.db.load(filename)
-        self.fill_tables_table()
-
-    def save_db(self):
-        filename = QtWidgets.QFileDialog.getSaveFileName(
-            self, 'Save database', '', 'Database Files (*.db)')[0]
-
-        if not filename:
-            return
-
-        self.db.save(filename)
-
-    def add_table(self):
-        if not self.add_table_window:
-            self.add_table_window = AddTableWindow(self)
-            self.add_table_window.setWindowFlag(QtCore.Qt.WindowStaysOnTopHint)
-            self.add_table_window.show()
-
-    def fill_tables_table(self):
-        table_names = self.db.get_table_names()
-        self.table_names_table.setRowCount(len(table_names))
-        for i, row in enumerate(table_names):
-            item = QtWidgets.QTableWidgetItem(row)
-            self.table_names_table.setItem(i, 0, item)
-
-    # TODO: show column data types as tooltips
-    def refill_table_data(self, row):
-        table_name = self.table_names_table.item(row, 0).text()
-        table = self.db.get_table(table_name)
-
-        headers = table.column_names
-        rows = table.data
-
-        self.table_data_table.clear()
-
-        self.table_data_table.setColumnCount(len(headers))
-        self.table_data_table.setHorizontalHeaderLabels(headers)
-        self.table_data_table.setRowCount(len(rows))
-
-        for i, row in enumerate(rows):
-            for j, value in enumerate(row):
-                item = QtWidgets.QTableWidgetItem(str(value))
-                self.table_data_table.setItem(i, j, item)
-
-    def closeEvent(self, a0):
-        if self.add_table_window:
-            self.add_table_window.close()
-        super().close()
-
-
-def run_GUI():
-    app = QtWidgets.QApplication(sys.argv)
-    w = DatabaseWindow()
-    w.show()
-    app.exec()
-
-
-def main():
-    run_GUI()
-
-
-if __name__ == '__main__':
-    def except_hook(cls, exception, traceback):
-        sys.__excepthook__(cls, exception, traceback)
-
-    sys.excepthook = except_hook
-    main()
---
+++ src/gui/main_window.py
@@ -0,0 +1,146 @@
+import sys
+
+from PyQt5 import QtCore, QtWidgets
+
+from db.database import Database
+from gui.add_table_window import AddTableWindow
+
+
+class ConfirmRemoval(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+
+class DatabaseWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.db = Database()
+
+        self.add_table_window = None
+        self.setup_UI()
+
+    def setup_UI(self):
+        self.resize(1139, 775)
+        # MainWindow.resize(800, 500)
+        self.centralwidget = QtWidgets.QWidget(self)
+
+        self.add_table_button = QtWidgets.QPushButton(self.centralwidget)
+        self.add_table_button.setGeometry(QtCore.QRect(30, 360, 71, 23))
+        self.add_table_button.setText('Add table')
+        self.add_table_button.clicked.connect(self.add_table)
+
+        # TODO: remove table function
+        # TODO: ask for confirmation
+        self.remove_table_button = QtWidgets.QPushButton(self.centralwidget)
+        self.remove_table_button.setGeometry(QtCore.QRect(30, 390, 91, 23))
+        self.remove_table_button.setText('Remove table')
+
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(220, 30, 801, 41))
+        self.search_button = QtWidgets.QPushButton(self.centralwidget)
+        self.search_button.setGeometry(QtCore.QRect(1030, 30, 71, 41))
+        self.search_button.setText("Search")
+
+        self.table_names_table = QtWidgets.QTableWidget(self.centralwidget)
+        self.table_names_table.setGeometry(QtCore.QRect(30, 30, 171, 321))
+        self.table_names_table.setColumnCount(1)
+        self.table_names_table.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self.table_names_table.cellDoubleClicked.connect(self.refill_table_data)
+        self.table_names_table.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
+
+        self.table_data_table = QtWidgets.QTableWidget(self.centralwidget)
+        self.table_data_table.setGeometry(QtCore.QRect(220, 80, 881, 661))
+        self.table_data_table.setColumnCount(0)
+        self.table_data_table.setRowCount(0)
+
+        self.load_db_button = QtWidgets.QPushButton(self.centralwidget)
+        self.load_db_button.setGeometry(QtCore.QRect(30, 670, 91, 31))
+        self.load_db_button.setText('Load database')
+        self.load_db_button.clicked.connect(self.load_db)
+
+        self.save_db_button = QtWidgets.QPushButton(self.centralwidget)
+        self.save_db_button.setGeometry(QtCore.QRect(30, 710, 91, 31))
+        self.save_db_button.setText('Save database')
+        self.save_db_button.clicked.connect(self.save_db)
+
+        self.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(self)
+        self.setStatusBar(self.statusbar)
+
+        self.setWindowTitle('Database Simulator')
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+    def load_db(self):
+        filename = QtWidgets.QFileDialog.getOpenFileName(
+            self, 'Load database', '', 'Database Files (*.db)')[0]
+
+        if not filename:
+            return
+
+        self.db.load(filename)
+        self.fill_tables_table()
+
+    def save_db(self):
+        filename = QtWidgets.QFileDialog.getSaveFileName(
+            self, 'Save database', '', 'Database Files (*.db)')[0]
+
+        if not filename:
+            return
+
+        self.db.save(filename)
+
+    def add_table(self):
+        if not self.add_table_window:
+            self.add_table_window = AddTableWindow(self)
+            self.add_table_window.setWindowFlag(QtCore.Qt.WindowStaysOnTopHint)
+            self.add_table_window.show()
+
+    def fill_tables_table(self):
+        table_names = self.db.get_table_names()
+        self.table_names_table.setRowCount(len(table_names))
+        for i, row in enumerate(table_names):
+            item = QtWidgets.QTableWidgetItem(row)
+            self.table_names_table.setItem(i, 0, item)
+
+    # TODO: show column data types as tooltips
+    def refill_table_data(self, row):
+        table_name = self.table_names_table.item(row, 0).text()
+        table = self.db.get_table(table_name)
+
+        headers = table.column_names
+        rows = table.data
+
+        self.table_data_table.clear()
+
+        self.table_data_table.setColumnCount(len(headers))
+        self.table_data_table.setHorizontalHeaderLabels(headers)
+        self.table_data_table.setRowCount(len(rows))
+
+        for i, row in enumerate(rows):
+            for j, value in enumerate(row):
+                item = QtWidgets.QTableWidgetItem(str(value))
+                self.table_data_table.setItem(i, j, item)
+
+    def closeEvent(self, a0):
+        if self.add_table_window:
+            self.add_table_window.close()
+        super().close()
+
+
+def run_GUI():
+    app = QtWidgets.QApplication(sys.argv)
+    w = DatabaseWindow()
+    w.show()
+    app.exec()
+
+
+def main():
+    run_GUI()
+
+
+if __name__ == '__main__':
+    def except_hook(cls, exception, traceback):
+        sys.__excepthook__(cls, exception, traceback)
+
+    sys.excepthook = except_hook
+    main()
